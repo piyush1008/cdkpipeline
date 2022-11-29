1899559786218b75a5d16aa299f0b4b511cc6abe
Piyush Dubey <piyush.dubey@hp.com>
2022-11-29 09:47:26 +0530

    fixing code in the pipeline

--- lib/cdkpipeline-stack.ts
+++ lib/cdkpipeline-stack.ts
@@ -15,11 +15,11 @@ export class CdkpipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'MyFirstPipeline', {
+    new CodePipeline(this, 'MyFirstPipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth',{
         input:CodePipelineSource.gitHub('piyush1008/cdkpipeline','main'),
-        commands:['npx npm ci',
+        commands:['npm ci',
                   'npm run build',
                   'npx cdk synth']
       }),
